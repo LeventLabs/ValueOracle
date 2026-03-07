282d3cffbefbd0bbf750b3567f59fd46e222c577
LeventLabs <levent@leventlabs.com>
2026-03-07 19:15:37 +0300

    fix: load dotenv in server.js for GROQ_API_KEY

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const marketplaceA = require('./sources/marketplaceA');
 const marketplaceB = require('./sources/marketplaceB');
 const marketplaceC = require('./sources/marketplaceC');
